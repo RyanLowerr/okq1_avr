d4153b42a978b2170a2e4d681351549091ab1498
Ryan Lowerr <ryan.lowerr@gmail.com>
2012-09-29 11:13:56 -0500

    started to roll up repetitive operations into loops

--- okq1.c
+++ okq1.c
@@ -10,18 +10,81 @@
 #include "ik/ik.h"
 #include "common.h"
 
+void servo_init(dynamixel* servo)
+{
+	servo[0].id  = FR_COXA_ID;
+	servo[1].id  = FR_FEMUR_ID;
+	servo[2].id  = FR_TIBIA_ID;
+	servo[3].id  = FR_TARSUS_ID;
+	servo[4].id  = BR_COXA_ID;
+	servo[5].id  = BR_FEMUR_ID;
+	servo[6].id  = BR_TIBIA_ID;
+	servo[7].id  = BR_TARSUS_ID;
+	servo[8].id  = BL_COXA_ID;
+	servo[9].id  = BL_FEMUR_ID;
+	servo[10].id = BL_TIBIA_ID;
+	servo[11].id = BL_TARSUS_ID;
+	servo[12].id = FL_COXA_ID;
+	servo[13].id = FL_FEMUR_ID;
+	servo[14].id = FL_TIBIA_ID;
+	servo[15].id = FL_TARSUS_ID;
+	servo[16].id = PAN_ID;
+	servo[17].id = TILT_ID;
+
+	servo[0].direction  =  1.0;
+	servo[1].direction  =  1.0;
+	servo[2].direction  = -1.0;
+	servo[3].direction  = -1.0;
+	servo[4].direction  =  1.0;
+	servo[5].direction  = -1.0;
+	servo[6].direction  =  1.0;
+	servo[7].direction  =  1.0;
+	servo[8].direction  =  1.0;
+	servo[9].direction  =  1.0;
+	servo[10].direction = -1.0;
+	servo[11].direction = -1.0;
+	servo[12].direction =  1.0;
+	servo[13].direction = -1.0;
+	servo[14].direction =  1.0;
+	servo[15].direction =  1.0;
+	servo[16].direction =  1.0;
+	servo[17].direction =  1.0;
+
+	servo[0].center  = -FR_COXA_ZERO;
+	servo[1].center  =  FEMUR_ZERO;
+	servo[2].center  = -TIBIA_ZERO;
+	servo[3].center  =  TARSUS_ZERO;
+	servo[4].center  =  BR_COXA_ZERO;
+	servo[5].center  =  FEMUR_ZERO;
+	servo[6].center  = -TIBIA_ZERO;
+	servo[7].center  =  TARSUS_ZERO;
+	servo[8].center  =  BL_COXA_ZERO;
+	servo[9].center  =  FEMUR_ZERO;
+	servo[10].center = -TIBIA_ZERO;
+	servo[11].center =  TARSUS_ZERO;
+	servo[12].center = -FL_COXA_ZERO;
+	servo[13].center =  FEMUR_ZERO;
+	servo[14].center = -TIBIA_ZERO;
+	servo[15].center =  TARSUS_ZERO;
+	servo[16].center =  PAN_ZERO;
+	servo[17].center =  TILT_ZERO;
+}
+
 int main(void)
 {
 	gaitdata gait;
+	dynamixel servo[NUM_SERVOS];
 	ikdata ikresults[4];	
 	float x[4], y[4], z[4];
 	uint8_t packet[54];
-
+	uint8_t n;
+	
+	servo_init(&servo[0]);
 	dynamixel_init();
 	gait_init(&gait, GAIT_TYPE_RIPPLE);
 	
 	while(1)
-	{		
+	{	
 		gait_process(&gait);
 		gait_increment(&gait);
 				
@@ -42,90 +105,49 @@ int main(void)
 		z[3] = FL_FOOT_Z_NEUTRAL + gait.z[3] * 30.0;
 		
 		ik_leg(x[0], y[0], z[0], &ikresults[0]);
+		servo[0].angle = ikresults[0].coxa;
+		servo[1].angle = ikresults[0].femur;
+		servo[2].angle = ikresults[0].tibia;
+		servo[3].angle = ikresults[0].tarsus;
+		
 		ik_leg(x[1], y[1], z[1], &ikresults[1]);
+		servo[4].angle = ikresults[1].coxa;
+		servo[5].angle = ikresults[1].femur;
+		servo[6].angle = ikresults[1].tibia;
+		servo[7].angle = ikresults[1].tarsus;
+		
 		ik_leg(x[2], y[2], z[2], &ikresults[2]);
+		servo[8].angle = ikresults[2].coxa;
+		servo[9].angle = ikresults[2].femur;
+		servo[10].angle = ikresults[2].tibia;
+		servo[11].angle = ikresults[2].tarsus;
+		
 		ik_leg(x[3], y[3], z[3], &ikresults[3]);
-	
-		ikresults[0].coxa   = (uint16_t) (AX_CENTER_VALUE + (ikresults[0].coxa - FR_COXA_ZERO) * 3.41);
-		ikresults[1].coxa   = (uint16_t) (AX_CENTER_VALUE + (ikresults[1].coxa + BR_COXA_ZERO) * 3.41);
-		ikresults[2].coxa   = (uint16_t) (AX_CENTER_VALUE + (ikresults[2].coxa + BL_COXA_ZERO) * 3.41);
-		ikresults[3].coxa   = (uint16_t) (AX_CENTER_VALUE + (ikresults[3].coxa - FL_COXA_ZERO) * 3.41);
-				
-		ikresults[0].femur  = (uint16_t) (AX_CENTER_VALUE + (ikresults[0].femur + FEMUR_ZERO) * 3.41);
-		ikresults[1].femur  = (uint16_t) (AX_CENTER_VALUE - (ikresults[1].femur + FEMUR_ZERO) * 3.41);
-		ikresults[2].femur  = (uint16_t) (AX_CENTER_VALUE + (ikresults[2].femur + FEMUR_ZERO) * 3.41);
-		ikresults[3].femur  = (uint16_t) (AX_CENTER_VALUE - (ikresults[3].femur + FEMUR_ZERO) * 3.41);
+		servo[12].angle = ikresults[3].coxa;
+		servo[13].angle = ikresults[3].femur;
+		servo[14].angle = ikresults[3].tibia;
+		servo[15].angle = ikresults[3].tarsus;
 		
-		ikresults[0].tibia  = (uint16_t) (AX_CENTER_VALUE - (ikresults[0].tibia - TIBIA_ZERO) * 3.41);
-		ikresults[1].tibia  = (uint16_t) (AX_CENTER_VALUE + (ikresults[1].tibia - TIBIA_ZERO) * 3.41);
-		ikresults[2].tibia  = (uint16_t) (AX_CENTER_VALUE - (ikresults[2].tibia - TIBIA_ZERO) * 3.41);
-		ikresults[3].tibia  = (uint16_t) (AX_CENTER_VALUE + (ikresults[3].tibia - TIBIA_ZERO) * 3.41);
+		servo[17].angle = 0;
+		servo[18].angle = 0;
 		
-		ikresults[0].tarsus = (uint16_t) (AX_CENTER_VALUE - (ikresults[0].tarsus + TARSUS_ZERO) * 3.41);
-		ikresults[1].tarsus = (uint16_t) (AX_CENTER_VALUE + (ikresults[1].tarsus + TARSUS_ZERO) * 3.41);
-		ikresults[2].tarsus = (uint16_t) (AX_CENTER_VALUE - (ikresults[2].tarsus + TARSUS_ZERO) * 3.41);
-		ikresults[3].tarsus = (uint16_t) (AX_CENTER_VALUE + (ikresults[3].tarsus + TARSUS_ZERO) * 3.41);
-		
-		packet[0]  = FR_COXA_ID;
-		packet[1]  = dynamixel_getlowbyte(ikresults[0].coxa);
-		packet[2]  = dynamixel_gethighbyte(ikresults[0].coxa);
-		packet[3]  = FR_FEMUR_ID;
-		packet[4]  = dynamixel_getlowbyte(ikresults[0].femur);
-		packet[5]  = dynamixel_gethighbyte(ikresults[0].femur);
-		packet[6]  = FR_TIBIA_ID;
-		packet[7]  = dynamixel_getlowbyte(ikresults[0].tibia);
-		packet[8]  = dynamixel_gethighbyte(ikresults[0].tibia);
-		packet[9]  = FR_TARSUS_ID;
-		packet[10] = dynamixel_getlowbyte(ikresults[0].tarsus);
-		packet[11] = dynamixel_gethighbyte(ikresults[0].tarsus);
-		
-		packet[12] = BR_COXA_ID;
-		packet[13] = dynamixel_getlowbyte(ikresults[1].coxa);
-		packet[14] = dynamixel_gethighbyte(ikresults[1].coxa);
-		packet[15] = BR_FEMUR_ID;
-		packet[16] = dynamixel_getlowbyte(ikresults[1].femur);
-		packet[17] = dynamixel_gethighbyte(ikresults[1].femur);
-		packet[18] = BR_TIBIA_ID;
-		packet[19] = dynamixel_getlowbyte(ikresults[1].tibia);
-		packet[20] = dynamixel_gethighbyte(ikresults[1].tibia);
-		packet[21] = BR_TARSUS_ID;
-		packet[22] = dynamixel_getlowbyte(ikresults[1].tarsus);
-		packet[23] = dynamixel_gethighbyte(ikresults[1].tarsus);
-		
-		packet[24] = BL_COXA_ID;
-		packet[25] = dynamixel_getlowbyte(ikresults[2].coxa);
-		packet[26] = dynamixel_gethighbyte(ikresults[2].coxa);
-		packet[27] = BL_FEMUR_ID;
-		packet[28] = dynamixel_getlowbyte(ikresults[2].femur);
-		packet[29] = dynamixel_gethighbyte(ikresults[2].femur);
-		packet[30] = BL_TIBIA_ID;
-		packet[31] = dynamixel_getlowbyte(ikresults[2].tibia);
-		packet[32] = dynamixel_gethighbyte(ikresults[2].tibia);
-		packet[33] = BL_TARSUS_ID;
-		packet[34] = dynamixel_getlowbyte(ikresults[2].tarsus);
-		packet[35] = dynamixel_gethighbyte(ikresults[2].tarsus);
-		
-		packet[36] = FL_COXA_ID;
-		packet[37] = dynamixel_getlowbyte(ikresults[3].coxa);
-		packet[38] = dynamixel_gethighbyte(ikresults[3].coxa);
-		packet[39] = FL_FEMUR_ID;
-		packet[40] = dynamixel_getlowbyte(ikresults[3].femur);
-		packet[41] = dynamixel_gethighbyte(ikresults[3].femur);
-		packet[42] = FL_TIBIA_ID;
-		packet[43] = dynamixel_getlowbyte(ikresults[3].tibia);
-		packet[44] = dynamixel_gethighbyte(ikresults[3].tibia);
-		packet[45] = FL_TARSUS_ID;
-		packet[46] = dynamixel_getlowbyte(ikresults[3].tarsus);
-		packet[47] = dynamixel_gethighbyte(ikresults[3].tarsus);
-		
-		packet[48] = PAN_ID;
-		packet[49] = dynamixel_getlowbyte(AX_CENTER_VALUE);
-		packet[50] = dynamixel_gethighbyte(AX_CENTER_VALUE);
-		packet[51] = TILT_ID;
-		packet[52] = dynamixel_getlowbyte(AX_CENTER_VALUE);
-		packet[53] = dynamixel_gethighbyte(AX_CENTER_VALUE);
-				
-		dynamixel_syncwrite(AX_GOAL_POSITION_L, 2, 18, &packet);
+		// calculate servo positions
+		for(uint8_t i = 0; i < NUM_SERVOS; i++)
+		{
+			servo[i].position = (uint16_t) (AX_CENTER_VALUE + (servo[i].direction * (servo[i].angle + servo[i].center) * 3.41));
+		}
+
+		// form dynamixel sync write data
+		n = 0;
+		for(uint8_t i = 0; i < NUM_SERVOS; i++)
+		{
+			packet[n++] = servo[i].id;
+			packet[n++] = dynamixel_getlowbyte(servo[i].position);
+			packet[n++] = dynamixel_gethighbyte(servo[i].position);
+		}
+
+		// write positions to servos
+		dynamixel_syncwrite(AX_GOAL_POSITION_L, 2, NUM_SERVOS, &packet);	
 		
 		_delay_ms(10);
 	}
