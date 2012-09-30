2eb3d5f56ea9919cf3cf368108138a6810459413
Ryan Lowerr <ryan.lowerr@gmail.com>
2012-09-29 22:18:19 -0500

    starting to add groundwork for controller control

--- okq1.c
+++ okq1.c
@@ -10,75 +10,109 @@
 #include "ik/ik.h"
 #include "common.h"
 
-void servo_init(dynamixel* servo)
+void joint_init(jointdata* joint)
 {
-	servo[0].id  = FR_COXA_ID;
-	servo[1].id  = FR_FEMUR_ID;
-	servo[2].id  = FR_TIBIA_ID;
-	servo[3].id  = FR_TARSUS_ID;
-	servo[4].id  = BR_COXA_ID;
-	servo[5].id  = BR_FEMUR_ID;
-	servo[6].id  = BR_TIBIA_ID;
-	servo[7].id  = BR_TARSUS_ID;
-	servo[8].id  = BL_COXA_ID;
-	servo[9].id  = BL_FEMUR_ID;
-	servo[10].id = BL_TIBIA_ID;
-	servo[11].id = BL_TARSUS_ID;
-	servo[12].id = FL_COXA_ID;
-	servo[13].id = FL_FEMUR_ID;
-	servo[14].id = FL_TIBIA_ID;
-	servo[15].id = FL_TARSUS_ID;
-	servo[16].id = PAN_ID;
-	servo[17].id = TILT_ID;
+	joint[0].id  = FR_COXA_ID;
+	joint[1].id  = FR_FEMUR_ID;
+	joint[2].id  = FR_TIBIA_ID;
+	joint[3].id  = FR_TARSUS_ID;
+	joint[4].id  = BR_COXA_ID;
+	joint[5].id  = BR_FEMUR_ID;
+	joint[6].id  = BR_TIBIA_ID;
+	joint[7].id  = BR_TARSUS_ID;
+	joint[8].id  = BL_COXA_ID;
+	joint[9].id  = BL_FEMUR_ID;
+	joint[10].id = BL_TIBIA_ID;
+	joint[11].id = BL_TARSUS_ID;
+	joint[12].id = FL_COXA_ID;
+	joint[13].id = FL_FEMUR_ID;
+	joint[14].id = FL_TIBIA_ID;
+	joint[15].id = FL_TARSUS_ID;
+	joint[16].id = PAN_ID;
+	joint[17].id = TILT_ID;
 
-	servo[0].direction  = FR_COXA_DIRECTION;
-	servo[1].direction  = FR_FEMUR_DIRECTION;
-	servo[2].direction  = FR_TIBIA_DIRECTION;
-	servo[3].direction  = FR_TARSUS_DIRECTION;
-	servo[4].direction  = BR_COXA_DIRECTION;
-	servo[5].direction  = BR_FEMUR_DIRECTION;
-	servo[6].direction  = BR_TIBIA_DIRECTION;
-	servo[7].direction  = BR_TARSUS_DIRECTION;
-	servo[8].direction  = BL_COXA_DIRECTION;
-	servo[9].direction  = BL_FEMUR_DIRECTION;
-	servo[10].direction = BL_TIBIA_DIRECTION;
-	servo[11].direction = BL_TARSUS_DIRECTION;
-	servo[12].direction = FL_COXA_DIRECTION;
-	servo[13].direction = FL_FEMUR_DIRECTION;
-	servo[14].direction = FL_TIBIA_DIRECTION;
-	servo[15].direction = FL_TARSUS_DIRECTION;
-	servo[16].direction = PAN_DIRECTION;
-	servo[17].direction = TILT_DIRECTION;
+	joint[0].direction  = FR_COXA_DIRECTION;
+	joint[1].direction  = FR_FEMUR_DIRECTION;
+	joint[2].direction  = FR_TIBIA_DIRECTION;
+	joint[3].direction  = FR_TARSUS_DIRECTION;
+	joint[4].direction  = BR_COXA_DIRECTION;
+	joint[5].direction  = BR_FEMUR_DIRECTION;
+	joint[6].direction  = BR_TIBIA_DIRECTION;
+	joint[7].direction  = BR_TARSUS_DIRECTION;
+	joint[8].direction  = BL_COXA_DIRECTION;
+	joint[9].direction  = BL_FEMUR_DIRECTION;
+	joint[10].direction = BL_TIBIA_DIRECTION;
+	joint[11].direction = BL_TARSUS_DIRECTION;
+	joint[12].direction = FL_COXA_DIRECTION;
+	joint[13].direction = FL_FEMUR_DIRECTION;
+	joint[14].direction = FL_TIBIA_DIRECTION;
+	joint[15].direction = FL_TARSUS_DIRECTION;
+	joint[16].direction = PAN_DIRECTION;
+	joint[17].direction = TILT_DIRECTION;
 
-	servo[0].center  = -FR_COXA_ZERO;
-	servo[1].center  =  FEMUR_ZERO;
-	servo[2].center  = -TIBIA_ZERO;
-	servo[3].center  =  TARSUS_ZERO;
-	servo[4].center  =  BR_COXA_ZERO;
-	servo[5].center  =  FEMUR_ZERO;
-	servo[6].center  = -TIBIA_ZERO;
-	servo[7].center  =  TARSUS_ZERO;
-	servo[8].center  =  BL_COXA_ZERO;
-	servo[9].center  =  FEMUR_ZERO;
-	servo[10].center = -TIBIA_ZERO;
-	servo[11].center =  TARSUS_ZERO;
-	servo[12].center = -FL_COXA_ZERO;
-	servo[13].center =  FEMUR_ZERO;
-	servo[14].center = -TIBIA_ZERO;
-	servo[15].center =  TARSUS_ZERO;
-	servo[16].center =  PAN_ZERO;
-	servo[17].center =  TILT_ZERO;
+	joint[0].center  = -FR_COXA_ZERO;
+	joint[1].center  =  FEMUR_ZERO;
+	joint[2].center  = -TIBIA_ZERO;
+	joint[3].center  =  TARSUS_ZERO;
+	joint[4].center  =  BR_COXA_ZERO;
+	joint[5].center  =  FEMUR_ZERO;
+	joint[6].center  = -TIBIA_ZERO;
+	joint[7].center  =  TARSUS_ZERO;
+	joint[8].center  =  BL_COXA_ZERO;
+	joint[9].center  =  FEMUR_ZERO;
+	joint[10].center = -TIBIA_ZERO;
+	joint[11].center =  TARSUS_ZERO;
+	joint[12].center = -FL_COXA_ZERO;
+	joint[13].center =  FEMUR_ZERO;
+	joint[14].center = -TIBIA_ZERO;
+	joint[15].center =  TARSUS_ZERO;
+	joint[16].center =  PAN_ZERO;
+	joint[17].center =  TILT_ZERO;
+}
+
+void foot_init(footdata* foot)
+{
+	foot[0].neutral_from_coxa_x = FOOT_X_NEUTRAL;
+	foot[0].neutral_from_coxa_y = FOOT_Y_NEUTRAL;
+	foot[0].neutral_from_center_x = FOOT_X_NEUTRAL + COXA_X_OFFSET;
+	foot[0].neutral_from_center_y = FOOT_Y_NEUTRAL + COXA_Y_OFFSET;
+	foot[0].neutral_z = -FOOT_Z_NEUTRAL;
+
+	foot[1].neutral_from_coxa_x = FOOT_X_NEUTRAL;
+	foot[1].neutral_from_coxa_y = -FOOT_Y_NEUTRAL;
+	foot[1].neutral_from_center_x = FOOT_X_NEUTRAL + COXA_X_OFFSET;
+	foot[1].neutral_from_center_y = -FOOT_Y_NEUTRAL + -COXA_Y_OFFSET;
+	foot[1].neutral_z = -FOOT_Z_NEUTRAL;
+	
+	foot[2].neutral_from_coxa_x = -FOOT_X_NEUTRAL;
+	foot[2].neutral_from_coxa_y = -FOOT_Y_NEUTRAL;
+	foot[2].neutral_from_center_x = -FOOT_X_NEUTRAL + -COXA_X_OFFSET;
+	foot[2].neutral_from_center_y = -FOOT_Y_NEUTRAL + -COXA_Y_OFFSET;
+	foot[2].neutral_z = -FOOT_Z_NEUTRAL;
+	
+	foot[3].neutral_from_coxa_x = -FOOT_X_NEUTRAL;
+	foot[3].neutral_from_coxa_y = FOOT_Y_NEUTRAL;
+	foot[3].neutral_from_center_x = -FOOT_X_NEUTRAL + -COXA_X_OFFSET;
+	foot[3].neutral_from_center_y = FOOT_Y_NEUTRAL + COXA_Y_OFFSET;
+	foot[3].neutral_z = -FOOT_Z_NEUTRAL;
 }
 
 int main(void)
 {
 	gaitdata gait;
-	dynamixel servo[NUM_SERVOS];	
-	float x[4], y[4], z[4];
+	jointdata joint[NUM_SERVOS];	
+	footdata foot[4];
+	float theta;
 	uint8_t packet[54];
 	uint8_t n;
 	
-	servo_init(&servo[0]);
+	float controller_x = 0.0;
+	float controller_y = 40.0;
+ 	float controller_z = 30.0;
+	float controller_r = 5.0;
+		
+	joint_init(&joint[0]);
+	foot_init(&foot[0]);
 	dynamixel_init();
 	gait_init(&gait, GAIT_TYPE_RIPPLE);
 	
@@ -86,40 +120,83 @@ int main(void)
 	{	
 		gait_process(&gait);
 		gait_increment(&gait);
-				
-		x[0] = FR_FOOT_X_NEUTRAL + gait.x[0] * 0.0;
-		y[0] = FR_FOOT_Y_NEUTRAL + gait.y[0] * 40.0;
-		z[0] = FR_FOOT_Z_NEUTRAL + gait.z[0] * 30.0;
 		
-		x[1] = BR_FOOT_X_NEUTRAL + gait.x[1] * 0.0;
-		y[1] = BR_FOOT_Y_NEUTRAL + gait.y[1] * 40.0;
-		z[1] = BR_FOOT_Z_NEUTRAL + gait.z[1] * 30.0;
+		if(controller_r != 0.0)
+		{
+			// Start with neutral foot position		
+			foot[0].x = foot[0].neutral_from_coxa_x;
+			foot[0].y = foot[0].neutral_from_coxa_y;
+			foot[0].z = foot[0].neutral_z;
 		
-		x[2] = BL_FOOT_X_NEUTRAL + gait.x[2] * 0.0;
-		y[2] = BL_FOOT_Y_NEUTRAL + gait.y[2] * 40.0;
-		z[2] = BL_FOOT_Z_NEUTRAL + gait.z[2] * 30.0;
+			foot[1].x = foot[1].neutral_from_coxa_x;
+			foot[1].y = foot[1].neutral_from_coxa_y;
+			foot[1].z = foot[1].neutral_z;
 		
-		x[3] = FL_FOOT_X_NEUTRAL + gait.x[3] * 0.0;
-		y[3] = FL_FOOT_Y_NEUTRAL + gait.y[3] * 40.0;
-		z[3] = FL_FOOT_Z_NEUTRAL + gait.z[3] * 30.0;
+			foot[2].x = foot[2].neutral_from_coxa_x;
+			foot[2].y = foot[2].neutral_from_coxa_y;
+			foot[2].z = foot[2].neutral_z;
+		
+			foot[3].x = foot[3].neutral_from_coxa_x;
+			foot[3].y = foot[3].neutral_from_coxa_y;
+			foot[3].z = foot[3].neutral_z;
+		}
+		else
+		{
+			// start with rotations from neutral foot position
+			theta = gait.r[0] * controller_r * 0.01745;
+			foot[0].x = foot[0].neutral_from_center_x * cos(theta) - foot[0].neutral_from_center_y * sin(theta);
+			foot[0].y = foot[0].neutral_from_center_x * sin(theta) + foot[0].neutral_from_center_y * cos(theta);
+		
+			theta = gait.r[1] * controller_r * 0.01745;
+			foot[1].x = foot[1].neutral_from_center_x * cos(theta) - foot[1].neutral_from_center_y * sin(theta);
+			foot[1].y = foot[1].neutral_from_center_x * sin(theta) + foot[1].neutral_from_center_y * cos(theta);
+		
+			theta = gait.r[2] * controller_r * 0.01745;
+			foot[2].x = foot[2].neutral_from_center_x * cos(theta) - foot[2].neutral_from_center_y * sin(theta);
+			foot[2].y = foot[2].neutral_from_center_x * sin(theta) + foot[2].neutral_from_center_y * cos(theta);
+		
+			theta = gait.r[3] * controller_r * 0.01745;
+			foot[3].x = foot[3].neutral_from_center_x * cos(theta) - foot[3].neutral_from_center_y * sin(theta);
+			foot[3].y = foot[3].neutral_from_center_x * sin(theta) + foot[3].neutral_from_center_y * cos(theta);
+		}
+		
+		if(controller_x != 0.0 || controller_x != 0.0 || controller_x != 0.0)
+		{
+			// Add gait translations to foot position
+			foot[0].x += gait.x[0] * controller_x;
+			foot[0].y += gait.y[0] * controller_y;
+			foot[0].z += gait.z[0] * controller_z;
+		
+			foot[1].x += gait.x[1] * controller_x;
+			foot[1].y += gait.y[1] * controller_y;
+			foot[1].z += gait.z[1] * controller_z;
+		
+			foot[2].x += gait.x[2] * controller_x;
+			foot[2].y += gait.y[2] * controller_y;
+			foot[2].z += gait.z[2] * controller_z;
+		
+			foot[3].x += gait.x[3] * controller_x;
+			foot[3].y += gait.y[3] * controller_y;
+			foot[3].z += gait.z[3] * controller_z;
+		}
 		
 		// ik calculations
 		for(uint8_t i = 0; i < 4; i++)
 		{
-			ik_leg(x[i], y[i], z[i], &servo[i*4].angle, &servo[i*4+1].angle, &servo[i*4+2].angle, &servo[i*4+3].angle);
+			ik_leg(foot[i].x, foot[i].y, foot[i].z, &joint[i*4].angle, &joint[i*4+1].angle, &joint[i*4+2].angle, &joint[i*4+3].angle);
 		}
 		
-		servo[16].angle = 0;
-		servo[17].angle = 0;
+		joint[16].angle = 0;
+		joint[17].angle = 0;
 
 		// form dynamixel sync write data
 		n = 0;
 		for(uint8_t i = 0; i < NUM_SERVOS; i++)
 		{
-			servo[i].position = (uint16_t) (AX_CENTER_VALUE + (servo[i].direction * (servo[i].angle + servo[i].center) * 3.41));
-			packet[n++] = servo[i].id;
-			packet[n++] = dynamixel_getlowbyte(servo[i].position);
-			packet[n++] = dynamixel_gethighbyte(servo[i].position);
+			joint[i].position = (uint16_t) (AX_CENTER_VALUE + (joint[i].direction * (joint[i].angle + joint[i].center) * 3.41));
+			packet[n++] = joint[i].id;
+			packet[n++] = dynamixel_getlowbyte(joint[i].position);
+			packet[n++] = dynamixel_gethighbyte(joint[i].position);
 		}
 
 		// write positions to servos
