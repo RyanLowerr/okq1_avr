650214d57f1a0652c64bdf6b46bc62a3a05915d3
Ryan Lowerr <ryan.lowerr@gmail.com>
2012-12-06 21:00:07 -0600

    gun servo defines added

--- common.h
+++ common.h
@@ -30,67 +30,82 @@ typedef struct
 #define TIBIA_LENGTH  68.0
 #define TARSUS_LENGTH 83.0
 
-#define NUM_SERVOS 18
+#define NUM_SERVOS 22
 
 // Servo ID's
-#define FR_COXA_ID   1
-#define FR_FEMUR_ID  2
-#define FR_TIBIA_ID  3
-#define FR_TARSUS_ID 4
+#define FR_COXA_ID     1
+#define FR_FEMUR_ID    2
+#define FR_TIBIA_ID    3
+#define FR_TARSUS_ID   4
 
-#define BR_COXA_ID   5
-#define BR_FEMUR_ID  6
-#define BR_TIBIA_ID  7
-#define BR_TARSUS_ID 8
+#define BR_COXA_ID     5
+#define BR_FEMUR_ID    6
+#define BR_TIBIA_ID    7
+#define BR_TARSUS_ID   8
 
-#define BL_COXA_ID   9
-#define BL_FEMUR_ID  10
-#define BL_TIBIA_ID  11
-#define BL_TARSUS_ID 12
+#define BL_COXA_ID     9
+#define BL_FEMUR_ID    10
+#define BL_TIBIA_ID    11
+#define BL_TARSUS_ID   12
 
-#define FL_COXA_ID   13
-#define FL_FEMUR_ID  14
-#define FL_TIBIA_ID  15
-#define FL_TARSUS_ID 16
+#define FL_COXA_ID     13
+#define FL_FEMUR_ID    14
+#define FL_TIBIA_ID    15
+#define FL_TARSUS_ID   16
 
-#define PAN_ID       17
-#define TILT_ID      18
+#define TURRET_PAN_ID  17
+#define TURRET_TILT_ID 18
+
+#define R_GUN_PAN_ID   19
+#define R_GUN_TILT_ID  20
+#define L_GUN_PAN_ID   21
+#define L_GUN_TILT_ID  22
 
 // Servo Direction
-#define FR_COXA_DIRECTION    1.0
-#define FR_FEMUR_DIRECTION   1.0
-#define FR_TIBIA_DIRECTION  -1.0
-#define FR_TARSUS_DIRECTION -1.0
+#define FR_COXA_DIRECTION     1.0
+#define FR_FEMUR_DIRECTION    1.0
+#define FR_TIBIA_DIRECTION   -1.0
+#define FR_TARSUS_DIRECTION  -1.0
 
-#define BR_COXA_DIRECTION    1.0
-#define BR_FEMUR_DIRECTION  -1.0
-#define BR_TIBIA_DIRECTION   1.0
-#define BR_TARSUS_DIRECTION  1.0
+#define BR_COXA_DIRECTION     1.0
+#define BR_FEMUR_DIRECTION   -1.0
+#define BR_TIBIA_DIRECTION    1.0
+#define BR_TARSUS_DIRECTION   1.0
 
-#define BL_COXA_DIRECTION    1.0
-#define BL_FEMUR_DIRECTION   1.0
-#define BL_TIBIA_DIRECTION  -1.0
-#define BL_TARSUS_DIRECTION -1.0
+#define BL_COXA_DIRECTION     1.0
+#define BL_FEMUR_DIRECTION    1.0
+#define BL_TIBIA_DIRECTION   -1.0
+#define BL_TARSUS_DIRECTION  -1.0
 
-#define FL_COXA_DIRECTION    1.0
-#define FL_FEMUR_DIRECTION  -1.0
-#define FL_TIBIA_DIRECTION   1.0
-#define FL_TARSUS_DIRECTION  1.0
+#define FL_COXA_DIRECTION     1.0
+#define FL_FEMUR_DIRECTION   -1.0
+#define FL_TIBIA_DIRECTION    1.0
+#define FL_TARSUS_DIRECTION   1.0
 
-#define PAN_DIRECTION        1.0
-#define TILT_DIRECTION       1.0
+#define TURRET_PAN_DIRECTION  1.0
+#define TURRET_TILT_DIRECTION 1.0
+
+#define R_GUN_PAN_DIRECTION   1.0
+#define R_GUN_TILT_DIRECTION  1.0
+#define L_GUN_PAN_DIRECTION   1.0
+#define L_GUN_TILT_DIRECTION  1.0
 
 // Servo 'Zero' angle
-#define FR_COXA_ZERO  45.0
-#define BR_COXA_ZERO  45.0
-#define BL_COXA_ZERO  135.0
-#define FL_COXA_ZERO  135.0
-#define FEMUR_ZERO    11.0
-#define TIBIA_ZERO    11.0
-#define TARSUS_ZERO   0.0
+#define FR_COXA_ZERO     45.0
+#define BR_COXA_ZERO     45.0
+#define BL_COXA_ZERO     135.0
+#define FL_COXA_ZERO     135.0
+#define FEMUR_ZERO       11.0
+#define TIBIA_ZERO       11.0
+#define TARSUS_ZERO      0.0
 
-#define PAN_ZERO  0.0
-#define TILT_ZERO 0.0
+#define TURRET_PAN_ZERO  0.0
+#define TURRET_TILT_ZERO 0.0
+
+#define R_GUN_PAN_ZERO   0.0
+#define R_GUN_TILT_ZERO  0.0
+#define L_GUN_PAN_ZERO   0.0
+#define L_GUN_TILT_ZERO  0.0
 
 // Foot neutral positions
 #define FOOT_X_NEUTRAL 90.0
--- okq1.c
+++ okq1.c
@@ -32,8 +32,12 @@ void joint_init(jointdata* joint)
 	joint[13].id = FL_FEMUR_ID;
 	joint[14].id = FL_TIBIA_ID;
 	joint[15].id = FL_TARSUS_ID;
-	joint[16].id = PAN_ID;
-	joint[17].id = TILT_ID;
+	joint[16].id = TURRET_PAN_ID;
+	joint[17].id = TURRET_TILT_ID;
+	joint[18].id = R_GUN_PAN_ID;
+	joint[19].id = R_GUN_TILT_ID;
+	joint[20].id = L_GUN_PAN_ID;
+	joint[21].id = L_GUN_TILT_ID;
 
 	joint[0].direction  = FR_COXA_DIRECTION;
 	joint[1].direction  = FR_FEMUR_DIRECTION;
@@ -51,8 +55,12 @@ void joint_init(jointdata* joint)
 	joint[13].direction = FL_FEMUR_DIRECTION;
 	joint[14].direction = FL_TIBIA_DIRECTION;
 	joint[15].direction = FL_TARSUS_DIRECTION;
-	joint[16].direction = PAN_DIRECTION;
-	joint[17].direction = TILT_DIRECTION;
+	joint[16].direction = TURRET_PAN_DIRECTION;
+	joint[17].direction = TURRET_TILT_DIRECTION;
+	joint[18].direction = R_GUN_PAN_DIRECTION;
+	joint[19].direction = R_GUN_TILT_DIRECTION;
+	joint[20].direction = L_GUN_PAN_DIRECTION;
+	joint[21].direction = L_GUN_TILT_DIRECTION;
 
 	joint[0].center  = -FR_COXA_ZERO;
 	joint[1].center  =  FEMUR_ZERO;
@@ -70,8 +78,12 @@ void joint_init(jointdata* joint)
 	joint[13].center =  FEMUR_ZERO;
 	joint[14].center = -TIBIA_ZERO;
 	joint[15].center =  TARSUS_ZERO;
-	joint[16].center =  PAN_ZERO;
-	joint[17].center =  TILT_ZERO;
+	joint[16].center =  TURRET_PAN_ZERO;
+	joint[17].center =  TURRET_TILT_ZERO;
+	joint[18].center =  R_GUN_PAN_ZERO;
+	joint[19].center =  R_GUN_TILT_ZERO;
+	joint[20].center =  L_GUN_PAN_ZERO;
+	joint[21].center =  L_GUN_TILT_ZERO;
 }
 
 // Write joint positions out to all servos using the dynamixel sync write command
