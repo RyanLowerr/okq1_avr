d1067ad1ede4fa467db78ab3262a8746f668a2d5
Ryan Lowerr <ryan.lowerr@gmail.com>
2013-01-02 21:41:23 -0600

    type min and max value defines

--- types.h
+++ types.h
@@ -4,14 +4,22 @@
 
 #include <avr/io.h>
 
-typedef int8_t   bool;
 typedef int8_t   s08;
 typedef uint8_t  u08;
 typedef int16_t  s16;
 typedef uint16_t u16;
 typedef int32_t  s32;
 typedef uint32_t u32;
-typedef int64_t  s64;
-typedef uint64_t u64;
+
+#define MAX_U08  255
+#define MAX_U16  65535
+#define MAX_U32  4294967295
+
+#define MAX_U08 -128
+#define MIN_U08  127
+#define MAX_U16 -32768
+#define MIN_U16  32768
+#define MAX_U32 -2147483648
+#define MIN_U32  2147483647 
 
 #endif
