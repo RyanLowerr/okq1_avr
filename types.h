c21502d77edd7a194f9d20fbdbd6f34340d7ba93
Ryan Lowerr <ryan.lowerr@gmail.com>
2013-01-02 21:42:44 -0600

    type min and max value defines

--- types.h
+++ types.h
@@ -15,11 +15,11 @@ typedef uint32_t u32;
 #define MAX_U16  65535
 #define MAX_U32  4294967295
 
-#define MAX_U08 -128
-#define MIN_U08  127
-#define MAX_U16 -32768
-#define MIN_U16  32768
-#define MAX_U32 -2147483648
-#define MIN_U32  2147483647 
+#define MIN_U08 -128
+#define MAX_U08  127
+#define MIN_U16 -32768
+#define MAX_U16  32767
+#define MIN_U32 -2147483648
+#define MAX_U32  2147483647 
 
 #endif
